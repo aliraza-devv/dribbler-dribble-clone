aa253acb065ee3e525ca9eb056bf43625344c1ec
Ali Raza <aliraza.dev.web@gmail.com>
2023-07-03 01:44:09 +0500

    header, footer, and grafbase setup

---
+++ grafbase/grafbase.config.ts
@@ -0,0 +1,25 @@
+import { g, auth, config } from '@grafbase/sdk'
+
+const User = g.model('User', {
+  name: g.string().length({min: 3, max: 30}),
+  email: g.string().unique(),
+  avatar: g.url(),
+  description: g.string().optional(),
+  githubUrl: g.url().optional(),
+  linkedInUrl: g.url().optional(),
+  projects: g.relation(() => Project).list().optional(),
+})
+
+const Project = g.model('Projects', {
+  title: g.string().length({min: 3}),
+  description: g.string(),
+  image: g.url(),
+  liveSiteUrl: g.url(),
+  githubUrl: g.url(),
+  category: g.string().search(),
+  createdBy: g.relation(() => User), 
+})
+
+export default config({
+  schema: g
+})
